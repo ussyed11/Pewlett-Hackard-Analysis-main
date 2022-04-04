54897c0b9374b6a7aa32ffdbdbe8337a06b8a33e
[Uzma Syed] <[uzsasyed@gmail.com]>
2022-04-04 11:45:15 -0400

    Adding sql files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,30 @@
+SELECT e.emp_no, e.first_name, e.last_name,
+	t.title, t.from_date, t.to_date
+INTO ret_title
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM ret_title;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM ret_title as rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
--- queries.sql
+++ queries.sql
@@ -29,3 +29,61 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--SELECT d.dept_name, 
+		--dm.emp_no, 
+		--dm.from_date,
+        -- dm.to_date
+--FROM departments as d
+--INNER JOIN dept_manager as dm
+--ON d.dept_no = dm.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no, ri.first_name,
+ri.last_name, de.to_date 
+FROM retirement_info as ri LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO ret_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+PH-EmployeeDB=# \copy ret_dept to '/Users/uzmasyed/Library/Mobile Documents/com~apple~CloudDocs/GW Data Science/Class Folder/Module 7 SQL/Analysis Projects Folder/Pewlett-Hackard-Analysis/Data/ret_dept.csv' csv header;
+
